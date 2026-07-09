b6fa9436d48bed4b575e3fa60e647a2565b88d9a
parth a <111271071+georgemonkey@users.noreply.github.com>
2026-07-09 11:18:11 -0700

    drivetrain chagnges

--- include/eternity_template/setup.hpp
+++ include/eternity_template/setup.hpp
@@ -13,8 +13,8 @@ extern rd::Selector selector;
 inline pros::Controller controller(pros::E_CONTROLLER_MASTER); //controller
 
 // drivetrain
-inline pros::MotorGroup right_dt({12,11,-15}, pros::MotorGearset::blue);    // right side
-inline pros::MotorGroup left_dt({20,14,-13}, pros::MotorGearset::blue);  // left side
+inline pros::MotorGroup right_dt({10,20}, pros::MotorGearset::blue);    // right side
+inline pros::MotorGroup left_dt({-1,-11}, pros::MotorGearset::blue);  // left side
 
 // drivetrain config for movement functions
 // (wheel size, width, rpm, etc.)
